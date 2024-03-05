3fe02a59b92d86fb41c4ee6c5b72cefafb5e258c
damian-garbarini <garbarinidamian@gmail.com>
2024-03-04 23:46:20 -0300

    cracion de paginas con parte del css

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,1 +1,17 @@
 console.log(tempData);
+
+    function createPeliculas(peliculas) {
+    const {title, year, director, duration, genre, rate, poster} = peliculas
+    const a = document.createElement("a")
+
+}
+
+function cargarPeliculas() {
+    const contenedor= document.getElementById("container")
+    contenedor.innerHTML ="";
+    const pelciulass = tempData[0];
+    const peliculas = pelciulass.map((peli)=>createPeliculas(peli));
+    return peliculas
+    peliculas.forEach((pelicula) =>  contenedor.appendChild(pelicula) );
+}
+console.log(cargarPeliculas());
